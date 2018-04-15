97be751ad4c2fb9e72836ea3a989c5fa92498f6f
puyangsky <puyangsky@pku.edu.cn>
2018-04-15 15:03:06 +0800

    init

---
+++ src/main/java/com/puyangsky/blog/controller/BlogController.java
@@ -0,0 +1,35 @@
+package com.puyangsky.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+/**
+ * Author:      puyangsky
+ * Date:        2018/4/15 下午12:02
+ */
+
+@Controller
+public class BlogController {
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "Welcome to Puyangsky's Blog";
+    }
+
+    @RequestMapping("/test")
+    public ModelAndView test(@RequestParam(name = "name")String name) {
+       ModelAndView mv = new ModelAndView("index");
+       mv.addObject("name", name);
+       return mv;
+    }
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("name", "Fuck");
+        return "index";
+    }
+}
