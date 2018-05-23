65fe9d0b46b181ed7969749db5d32e4055631bc6
puyangsky <puyangsky@pku.edu.cn>
2018-05-23 23:52:22 +0800

    add user

--- blog.sql
+++ blog.sql
@@ -1,16 +1,39 @@
--- MySQL dump 10.13  Distrib 5.7.17, for osx10.11 (x86_64)
---
--- Host: localhost    Database: test
--- ------------------------------------------------------
--- Server version	5.7.17
+# article table DDL
+CREATE TABLE article
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255),
+  content TEXT,
+  author VARCHAR(255),
+  publish_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+# tag table DDL
+CREATE TABLE tag
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(32) NOT NULL,
+  article_num INT(11) DEFAULT '0'
+);
+CREATE UNIQUE INDEX name ON tag (name);
+CREATE UNIQUE INDEX name_2 ON tag (name);
+
+
+# article_tag_relationship table DDL
+CREATE TABLE article_tag_relationship
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  article_id INT(11) NOT NULL,
+  tag_id INT(11) NOT NULL
+);
+
+# user table DDL
+CREATE TABLE user
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  username VARCHAR(32) NOT NULL,
+  password VARCHAR(128),
+  role VARCHAR(32)
+);
+CREATE UNIQUE INDEX username ON user (username);
---
+++ src/main/java/com/puyangsky/blog/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.puyangsky.blog.dao;
+
+/**
+ * Author:      puyangsky
+ * Date:        2018/5/23 下午2:49
+ */
+public interface UserDao {
+}
